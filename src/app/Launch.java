e30030956dab18aa093813675f80c00524881559
YRQD <amirashraf012080@gmail.com>
2026-03-22 15:41:31 +0200

    storing logs at user's home

--- src/app/Launch.java
+++ src/app/Launch.java
@@ -3,7 +3,6 @@ import infrastructure.persistence.DatabaseConnection;
 
 public class Launch {
     public static void main(String[] args) {
-        System.setProperty("MY_APP_PATH", "A:\\Store_Manager_App"); // TODO: Remove this line before deployment.
         DatabaseConnection.startConnection();
         javax.swing.SwingUtilities.invokeLater(() -> {
             presentation.login.LoginFrame login = new presentation.login.LoginFrame("PRODUCTS");
